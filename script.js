7399cdf9a671c7c67a2b23650b92f4bea8118019
RedonMehmeti <redonmehmeti2019@gmail.com>
2024-02-17 08:25:27 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+let titulli = document.getElementById('header')
+let fotoja = document.querySelector('img')
+let paragrafi1 = document.querySelector('.firstP')
+let paragrafi2 = document.querySelector('.secondP')
+let from = document.getElementById('from')
+
+titulli.textContent = 'Urime ditelindjen ! '
+fotoja.src = 'https://media.istockphoto.com/id/1329407781/vector/birthday-vector-background-with-colorful-confetti-and-serpentine-ribbons-isolated-on-dark.jpg?s=612x612&w=0&k=20&c=I6SHjjS5l76YxsCJq51JPlM5xPKM2zwn_r3GHDD5_Ak='
+fotoja.alt = 'Foto e insertuar me java script'
+paragrafi1.textContent = 'Urime ditelindja ishalla dhe 100 me famile dhe te gjitha te mirat'
+from.textContent = 'Nga:Redon Mehmeti'
+paragrafi2.textContent = 'Te uroj te gjitha te mirat !'
+
+// let titulli = document.getElementById('header')
+
+// titulli.style.fontstyle = "italic"
+// titulli.style.color = "#cecece"
+// titulli.style.backgroundColor = "yellow"
+// titulli.style.textDecoration = "underline"
+
+// titulli.classList.add("reda")
+// titulli.classList.remove("reda")
